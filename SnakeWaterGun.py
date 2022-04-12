a25ec9da73e53ef98cd5e564643ac08b2383c47b
KingSenpai <91060229+ChintuWhitehatjunior@users.noreply.github.com>
2022-04-12 10:46:35 +0530

    Update SnakeWaterGun.py

--- SnakeWaterGun.py
+++ SnakeWaterGun.py
@@ -30,13 +30,19 @@ else:
     comp = "Gun"
 
 print("Computer's turn:- Computer chose")
-you = input("Your turn:- Snake, Water or Gun?(Type any one of them)")
+you = input("Your turn:- Snake, Water or Gun?(Type any one of them)")  
 a = game(comp, you)
 print(f"Computer chose {comp}")
 print(f"You chose {you}")
-if a == None:
+if a != "Snake":
+    print("Enter a valid option and try again!")
+elif a != "Water":
+    print("Enter a valid option and try again!")
+elif a != "Gun":
+    print("Enter a valid option and try again!") 
+elif a == None:
     print("The game is a tie!")
 elif a:
-    print("You won!")
+    print("You won!")   
 else:
     print("you lost lmao nubre")
